8945165fc9c82b6069ff6dbb145210c14b195c45
derekmarks <36017560+derekmarks@users.noreply.github.com>
2020-04-16 13:23:16 -0400

    Update funct.py

--- funct.py
+++ funct.py
@@ -9,7 +9,7 @@ import os
 import math
 import imutils
 
-#THRESH_VAL = 180 #Best for image 4
+
 THRESH_VAL = 203 #Best for all other images
 SCALE_VAL = 110
 
@@ -50,8 +50,8 @@ def threshold(img):
     ret,img = cv2.threshold(img,THRESH_VAL,255,cv2.THRESH_BINARY)   #3. Threshold
     return img                                                      #4. Return
 
-def runwayWidth(markerCount):
-    return (markerCount*1.95)+(markerCount*1.8)
+def runwayWidth(markerCount):                            #Takes the number of markers
+    return (markerCount*1.95)+(markerCount*1.8)          #Returns the width of the airefeild in meters
 
 
 def getCentroid(c):
@@ -88,43 +88,40 @@ def rangeClassifier(visual,thresh):        #Draw contours and lable with number 
         cv2.putText(visual, "{}".format(c.size), point, cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 0), 2) #Write number of vertexes
     return visual
 
-def rectArea(rect):
+def rectArea(rect):                             #Takes a rect object, returns its area A=(WxH)
     return rect[1][0]*rect[1][1]
 
-def bndBoxClassifier(visual,thresh):        #Draw contours and lable with number of corners
+def bndBoxClassifier(visual,thresh):        #Classifies Chevrons from threshold markings
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)   #1. Calculate Contours
     cnts = imutils.grab_contours(cnts)                                                  #2. Extract contour list from data object
 
     rects = []                                                                          #an array to store all relevant bounding boxes
     widthMarkersCenter = []
-    for c in cnts: #Draw The contours                                                   #3. For each contour
+    for c in cnts: #Draw The contours                                       #3. For each contour
         if c.size>300 and c.size<670:
-            cv2.drawContours(visual, [c], -1, (0, 255, 0), 2)                               #Draw the Contour on the origonal image
-            rect = cv2.minAreaRect(c)
+            cv2.drawContours(visual, [c], -1, (0, 255, 0), 2)               #3.1 Draw the Contour on the origonal image
+            rect = cv2.minAreaRect(c)                                       #3.2 Calclulate the min area bounding box
             rects.append(rect)
     mean=0
-    for rect in rects:#Find the mean
-        mean=mean+rectArea(rect) #add the area of the current box to the sum
-    mean = mean / len(rects)
+    for rect in rects:                                                      #4.1 Find the mean
+        mean=mean+rectArea(rect)                                            #4.2 add the area of the current box to the sum
+    mean = mean / len(rects)                                                #4.3 divide by num bounding boxes to find mean
     for rect in rects:
-        box = cv2.boxPoints(rect)   #generates an narray of points representing rect
-        box = np.int0(box)  #ALias to 64 bit integer for iterating
+        box = cv2.boxPoints(rect)       #generates an narray of points representing rect
+        box = np.int0(box)              #Alias to 64 bit integer for iterating
         #Catagorize and draw
-        if(rect[1][0]*rect[1][1])<mean*1.1:#Classified as width markers
-            cv2.drawContours(visual,[box],0,(0,0,255),2)
-            widthMarkersCenter.append(rect[0])#[0]is center, [1]is dimensions, [2]is angle
-        else:#Classified as Arrows
-            cv2.drawContours(visual,[box],0,(255,0,0),2)
-            #rects.remove(rect)  #It isnt a widht marker, so get rid of it
+        if(rect[1][0]*rect[1][1])<mean*1.1:     #Classified as threshold markers
+            cv2.drawContours(visual,[box],0,(0,0,255),2)        #Draw the bounding box in red
+            widthMarkersCenter.append(rect[0])                  #Save the centroid of the contour group
+        else:                                   #Classified as chevrons
+            cv2.drawContours(visual,[box],0,(255,0,0),2)        #Draw the bounding box in blue
+   
 
 
-    #Only width markers are left in rects
-    widthCount=len(widthMarkersCenter)
-    if(widthCount%2!=0):
+    
+    widthCount=len(widthMarkersCenter)  #Count the number of width markers in the width marker array
+    if(widthCount%2!=0):                #If there is an odd number of width markers, then round up
         widthCount=widthCount+1 #Must be an even number of width markers, corrects for one not picked up due to intersection with other markings
-    bRCorner = (50,visual.shape[1]-50)
+    bRCorner = (50,visual.shape[1]-50)  #Get the bottom right corner of the image
     cv2.putText(visual, "{} markers, width is {} m".format(widthCount, runwayWidth(widthCount)), bRCorner, cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 0), 2)     #Count them, write width at bottom of screen
-    return visual
-
-    #size
-    #angle
+    return visual       #write the runway width on the images bottom right corner, and return it
