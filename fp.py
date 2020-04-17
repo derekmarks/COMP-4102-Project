51b0d5ee1947d044308b1913dda99c8602eebd1d
derekmarks <36017560+derekmarks@users.noreply.github.com>
2020-04-16 23:24:26 -0400

    Comments

--- fp.py
+++ fp.py
@@ -10,18 +10,18 @@ def visContours(windowName,img):
     cv2.imshow(windowName+"_Thresh",thresh)    #Show the thresholded image
     cv2.imshow(windowName, contour)             #Show the contour image
 
-img_1 = loadSample_1()
+img_1 = loadSample_1()  #Quickly load some image samples wihout using the template finder, for sake of time
 img_2 = loadSample_2()
 img_3 = loadSample_3()
 img_4 = loadSample_4()
 
-visContours("Img_1",img_1)
+visContours("Img_1",img_1) #Display the output of the classifier on sampel images
 visContours("Img_2",img_2)
 visContours("Img_3",img_3)
 visContours("Img_4",img_4)
 
 
-img_5, startX, startY, endX, endY = matchit(os.getcwd()+"\\aerials\\aerial_1-r.tif", False)
+img_5, startX, startY, endX, endY = matchit(os.getcwd()+"\\aerials\\aerial_1-r.tif", False) #Load one image using the template locator as proof of concept
 print("Y: ", (endY +200) - (startY -200) , " X: ", (endX +200) - (startX -200) )
 img_5 = img_5[startY -200 :endY +200 , startX -200 :endX +200]
 img_5 = imutils.rotate_bound(img_5, 60)
