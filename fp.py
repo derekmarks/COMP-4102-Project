fcf260d461c8082ed684692b19e4406124ce4297
TheVelocityRaptor <35236734+TheVelocityRaptor@users.noreply.github.com>
2020-04-16 22:09:30 -0400

    Clean up

--- fp.py
+++ fp.py
@@ -6,9 +6,9 @@ def visContours(windowName,img):
     thresh = threshold(img)    #Threshold the image
     contour = bndBoxClassifier(img,thresh)      #The final method for counting threshold markings
 
-    contour = rangeClassifier(img,thresh)      #Shows how contour set length can be used to filter out undesriable markings
-    contour = visCntsCorners(img,thresh)       #Shows contour detection after thresholding and number of vertex in each contour
-    cv2.imshow(windowName+"_Thresh",thresh)    #Show the thresholded image
+    #contour = rangeClassifier(img,thresh)      #Shows how contour set length can be used to filter out undesriable markings
+    #contour = visCntsCorners(img,thresh)       #Shows contour detection after thresholding and number of vertex in each contour
+    #cv2.imshow(windowName+"_Thresh",thresh)    #Show the thresholded image
     cv2.imshow(windowName, contour)             #Show the contour image
 
 img_1 = loadSample_1()
@@ -22,22 +22,13 @@ visContours("Img_2",img_2)
 visContours("Img_3",img_3)
 visContours("Img_4",img_4)
 
-#img_5 = matchit(os.getcwd()+"\\images\\chrome_nhCeo1xNis.jpg")
-angle = rotateit(os.getcwd()+"\\aerials\\aerial_1-r.tif", 50)
-img_5, startX, startY, endX, endY = matchit(os.getcwd()+"\\aerials\\aerial_1-r.tif", False)
-print("Y: ", (endY +200) - (startY -200) , " X: ", (endX +200) - (startX -200) )
-img_5 = img_5[startY -200 :endY +200 , startX -200 :endX +200]
-img_5 = imutils.rotate_bound(img_5, 60)
-#img_5 = img_5[startY :endY , startX :endX]  
-cv2.imshow("img_5", img_5)
-cv2.waitKey(0)
-visContours("Img_5",img_5) #This is broken because a div by zero occurs in funct line 108
+
+img_5, startX, startY, endX, endY = matchit(os.getcwd()+"\\aerials\\aerial_1-r.tif", False, 0) # Runs the matchit function. This template matches and returns the cordinates of a box that best correlates to the templates
+img_5 = img_5[startY -200 :endY +200 , startX -200 :endX +200] # Takes the box of best correlated, expands its size to include more context in the image and then crops out the rest
+img_5 = imutils.rotate_bound(img_5, 60) # Rotates it 60 degrees to match the original aerial_1
+
+visContours("Img_5",img_5) # Runs the croped and scaled image through the Countours to find its shape
 
 cv2.waitKey(0)
 
-#img_6, startX, startY, endX, endY = matchit(os.getcwd()+"\\aerials\\aerial_1-r.tif")
-#img_6 = img_6[startY:endY,startX:endX] 
-#img_6 = scale_image(img_6,110)   
-#cv2.imshow("img_6", img_6)
 
-#cv2.waitKey(0)
