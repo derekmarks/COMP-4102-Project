c5d705c4f480f2cead8d861bc1f533770543af09
derekmarks <36017560+derekmarks@users.noreply.github.com>
2020-04-16 23:22:51 -0400

    Displays all imagery

--- fp.py
+++ fp.py
@@ -10,6 +10,16 @@ def visContours(windowName,img):
     cv2.imshow(windowName+"_Thresh",thresh)    #Show the thresholded image
     cv2.imshow(windowName, contour)             #Show the contour image
 
+img_1 = loadSample_1()
+img_2 = loadSample_2()
+img_3 = loadSample_3()
+img_4 = loadSample_4()
+
+visContours("Img_1",img_1)
+visContours("Img_2",img_2)
+visContours("Img_3",img_3)
+visContours("Img_4",img_4)
+
 
 img_5, startX, startY, endX, endY = matchit(os.getcwd()+"\\aerials\\aerial_1-r.tif", False)
 print("Y: ", (endY +200) - (startY -200) , " X: ", (endX +200) - (startX -200) )
